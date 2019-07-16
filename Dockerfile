15e00288bec1dbe62b9a937f0365fbfd41d162ec
raphaellschettino <raphaelluiz128@hotmail.com>
2019-07-16 13:30:50 -0300

    conexao com api

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,11 @@ FROM node:alpine
 WORKDIR /usr/app
 
 COPY package*.json ./
+RUN npm install -g grunt-cli bower yo generator-karma generator-angular
 RUN npm install --quiet
 
 
 COPY . .
 
 EXPOSE 9000
-CMD grunt serve
+CMD ["grunt", "serve"]
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,16 @@
  * # MainCtrl
  * Controller of the frontEadApp
  */
-angular.module('frontEadApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+ 
+var app = angular.module('frontEadApp',['datatables']);
+app.controller('crudController',function ($scope,$http){
+  $scope.fetchData = function (){
+    $http.get('https://apiead.herokuapp.com/api/clientes').success(function(data){
+     $scope.namesData = data;
+     
+});
+  };
+});
+  
+
