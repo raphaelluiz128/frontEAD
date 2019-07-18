0efbd9e44fae5d331790da26d2834eb75846370b
raphaellschettino <raphaelluiz128@hotmail.com>
2019-07-17 23:54:32 -0300

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+const cors = require('cors');
+app.use(cors({}));
 app.use(express.static(__dirname));
 app.listen(port);
