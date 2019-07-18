4f5a1e4d513dc71397006ad274f111324fd9ded8
raphaellschettino <raphaelluiz128@hotmail.com>
2019-07-18 00:37:27 -0300

    configuraçao heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 app.use(cors({}));
 const server = require('http').Server(app);
 console.log('ok');
-app.use(express.static(__dirname+'/'));
+app.use(express.static(__dirname+'/app/index.html'));
 server.listen((process.env.PORT || 3200), function(){
-    console.log('listening on',process.env.PORT);
+    console.log(__dirname+'/app/index.html');
   });
