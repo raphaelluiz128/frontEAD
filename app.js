0f7cc45d67dd3bbe4b5bd3b0affa322608972dac
raphaellschettino <raphaelluiz128@hotmail.com>
2019-07-18 01:47:34 -0300

    configuraçao heroku dist

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 const cors = require('cors');
 app.use(cors({}));
 console.log('ok');
-app.use(express.static('./app/index.html'));
+app.use(express.static(__dirname+'/app/index.html'));
 app.listen((process.env.PORT || 3200), function(){
-    console.log(__dirname+'/index.html');
+    console.log(__dirname+'/app/index.html');
   });
