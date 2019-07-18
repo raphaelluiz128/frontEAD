35e6e947bc42f7136fb83c99d7929a643eef41b0
raphaellschettino <raphaelluiz128@hotmail.com>
2019-07-18 01:51:39 -0300

    configuraçao heroku dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: 'https://frontead.herokuapp.com/',
         livereload: 35729
       },
       livereload: {
